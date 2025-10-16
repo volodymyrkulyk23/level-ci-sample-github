e6441833ef0791b658cfcd963ff3b0c39a1f0a6c
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2025-10-16 18:29:04 +0300

    CA-5603

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -7,3 +7,20 @@ export default {
   reportPaths: ["./level-ci-reports"],
   server: "https://api.dev.userway.dev",
 } satisfies Config;
+
+
+//ngrok
+// import type { Config } from '@level-ci/cli'
+// import { ngrokConnection } from '@level-ci/ngrok'
+ 
+// export default {
+//   organization: 'volodymyr-kulyk-40-userway-org-lando',
+//   project: 'sample-project-ngrok',
+//   token: process.env.LEVEL_CI_TOKEN,
+//   server: "https://api.dev.userway.dev",
+//   connection: ngrokConnection({
+//     addr: 'http://localhost:1342',
+//     authtoken: "2hpftK26ATCj6DnGdXhcpoRnLDB_3Sda24QiS8fGYYRWTFgxd",
+//   }),
+//   pages: [{ url: '/article.html' }],
+// } satisfies Config
