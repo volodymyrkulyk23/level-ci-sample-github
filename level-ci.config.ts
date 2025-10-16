04c88170dd32ec6d1d053bfbfd8daa2ce04494dd
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2025-10-16 18:40:01 +0300

    report file removed - check

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -6,7 +6,7 @@ export default {
   token: process.env.LEVEL_CI_TOKEN,
   reportPaths: ["./level-ci-reports"],
   server: "https://api.dev.userway.dev",
-} satisfies Config;
+} satisfies Config; 
 
 //ngrok
 // import type { Config } from '@level-ci/cli'
