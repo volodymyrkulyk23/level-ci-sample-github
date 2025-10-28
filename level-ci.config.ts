6ac88201316d7e0e6c3d74cdf30cda2fc452a0b6
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2025-10-28 19:36:30 +0200

    main - prod - new packages check

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,25 +1,33 @@
-import type { Config } from "@level-ci/cli";
+// import type { Config } from "@level-ci/cli";
 
-export default {
-  organization: "volodymyr-kulyk-32-userway-org-podss",
-  project: "level-ci-sample-github",
-  token: process.env.LEVEL_CI_TOKEN,
-  reportPaths: ["./level-ci-reports"],
-  server: "https://api.qa.userway.dev",
-} satisfies Config; 
+// export default {
+//   organization: "volodymyr-kulyk-32-userway-org-podss",
+//   project: "level-ci-sample-github",
+//   token: process.env.LEVEL_CI_TOKEN,
+//   reportPaths: ["./level-ci-reports"],
+//   server: "https://api.qa.userway.dev",
+// } satisfies Config; 
 
 //ngrok
 // import type { Config } from '@level-ci/cli'
 // import { ngrokConnection } from '@level-ci/ngrok'
  
 // export default {
-//   organization: 'volodymyr-kulyk-40-userway-org-lando',
-//   project: 'sample-project-ngrok',
+//   organization: 'volodymyr-kulyk-1-levelaccess-com-clkuq',
+//   project: '2',
 //   token: process.env.LEVEL_CI_TOKEN,
-//   server: "https://api.dev.userway.dev",
 //   connection: ngrokConnection({
 //     addr: 'http://localhost:1342',
 //     authtoken: "2hpftK26ATCj6DnGdXhcpoRnLDB_3Sda24QiS8fGYYRWTFgxd",
 //   }),
 //   pages: [{ url: '/article.html' }],
 // } satisfies Config
+
+// PROD
+import type { Config } from "@level-ci/cli";
+export default {
+ organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
+ project: "a11y-webdriverio-sample",
+ token: process.env.LEVEL_CI_TOKEN,
+ reportPaths: ['./level-ci-reports']
+} satisfies Config;
