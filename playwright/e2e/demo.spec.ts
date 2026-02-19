8e86c826c6154d30e5c616964b1658df1941501a
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-02-19 14:00:22 +0200

    main - 1  page rescanned

--- playwright/e2e/demo.spec.ts
+++ playwright/e2e/demo.spec.ts
@@ -3,12 +3,12 @@ import { levelAnalyze } from "@level-ci/a11y-playwright";
 
 test.describe("navigation", () => {
   test("runs Level CI analysis on the home page", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("/article.html");
 
-    await expect(page.locator(".headline")).toContainText(/Best Broadband/);
-    await expect(page.locator(".headline")).toContainText(
+    // await expect(page.locator(".headline")).toContainText(/Best Broadband/);
+    // await expect(page.locator(".headline")).toContainText(
       /Internet Plans For You/
-    );
+    // );
     await levelAnalyze(page);
     // Run the accessibility analysis and write a report to ./level-ci-reports
     // await levelAnalyze(page);
