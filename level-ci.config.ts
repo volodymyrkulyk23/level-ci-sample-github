d5290cbeac9c0f364652ecd7e3a0093dd6467220
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-03-24 23:27:38 +0200

    CA-6538 - hotfix verification

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "@level-ci/cli";
 
 export default {
-  organization: "volodymyr-kulyk-32-userway-org-podss",
+  organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
   project: "level-ci-sample-github",
   token: process.env.LEVEL_CI_TOKEN,
   reportPaths: ["./level-ci-reports"],
-  server: "https://staging.uw.ci.levelaccess.io",
+  override: {
+    "br21": {
+      scope: "overall",
+    },
+  },
 } satisfies Config;
 
 // ngrok
