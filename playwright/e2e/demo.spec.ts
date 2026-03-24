81119ccd56cbf1d928761ab011923e5bcb813cfe
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-03-24 23:19:11 +0200

    CA-6538 - fix check

--- playwright/e2e/demo.spec.ts
+++ playwright/e2e/demo.spec.ts
@@ -3,14 +3,12 @@ import { levelAnalyze } from "@level-ci/a11y-playwright";
 
 test.describe("navigation", () => {
   test("runs Level CI analysis on the home page", async ({ page }) => {
-    await page.goto("/article.html");
+    await page.goto("/");
 
     // await expect(page.locator(".headline")).toContainText(/Best Broadband/);
     // await expect(page.locator(".headline")).toContainText(
       /Internet Plans For You/
     // );
     await levelAnalyze(page, {});
-    // Run the accessibility analysis and write a report to ./level-ci-reports
-    // await levelAnalyze(page);
   });
 });
